6519d1d04bab625f7f162761c2bbc37169dbccef
Travis CI <travis@travis-ci.org>
2018-01-20 10:22:43 +0000

    Travis CI build https://travis-ci.org/rainchen/code_quality/builds/331152482 pushed to gh-pages

--- quality_audit/metric_fu/results/flog.js
+++ quality_audit/metric_fu/results/flog.js
@@ -1,3 +1,3 @@
         var graph_title = 'Flog: code complexity';
-        var graph_series = [{name: 'average', data: [12.152464967908692]},{name: 'top 5% average', data: [78.47345485054771]}];
-        var graph_labels = {"0":"1/19"};
+        var graph_series = [{name: 'average', data: [12.184824580007344]},{name: 'top 5% average', data: [78.26992767756373]}];
+        var graph_labels = {"0":"1/20"};
--- quality_audit/metric_fu/results/stats.js
+++ quality_audit/metric_fu/results/stats.js
@@ -1,3 +1,3 @@
         var graph_title = 'Stats: LOC & LOT';
-        var graph_series = [{name: 'LOC', data: [12]},{name: 'LOT', data: [44]}];
-        var graph_labels = {"0":"1/19"};
+        var graph_series = [{name: 'LOC', data: [12]},{name: 'LOT', data: [88]}];
+        var graph_labels = {"0":"1/20"};
