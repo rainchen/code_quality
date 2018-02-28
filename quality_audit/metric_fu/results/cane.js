3ef679ddfc2f74239e98ab1625af937e2d35b6cb
Travis CI <travis@travis-ci.org>
2018-02-28 07:25:15 +0000

    Travis CI build https://travis-ci.org/rainchen/code_quality/builds/347149091 pushed to gh-pages

--- quality_audit/metric_fu/results/cane.js
+++ quality_audit/metric_fu/results/cane.js
@@ -1,3 +1,3 @@
         var graph_title = 'Cane: code quality threshold violations';
         var graph_series = [{name: 'cane', data: [26]}];
-        var graph_labels = {"0":"2/1"};
+        var graph_labels = {"0":"2/28"};
--- quality_audit/metric_fu/results/flog.js
+++ quality_audit/metric_fu/results/flog.js
@@ -1,3 +1,3 @@
         var graph_title = 'Flog: code complexity';
         var graph_series = [{name: 'average', data: [11.647976882611749]},{name: 'top 5% average', data: [63.56981082231283]}];
-        var graph_labels = {"0":"2/1"};
+        var graph_labels = {"0":"2/28"};
